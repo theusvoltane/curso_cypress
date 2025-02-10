ba04b7adc93da8da1911341b7c90b380be2ca08a
theusvoltane <m.voltane@gmail.com>
2025-02-10 20:13:41 -0300

    remoção do token no teste

--- cypress/e2e/exercicio_4_1.cy.js
+++ cypress/e2e/exercicio_4_1.cy.js
@@ -1,7 +1,5 @@
 describe('exercicio 41', () => {
 
-  const authorization = 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJjNDJiOGMxMy0xNDc0LTQ3ZGYtYjRjZC1lZDg3ZmNlNDE5NWMiLCJhZG9wdGVyTmFtZSI6Ik1hdGhldXMgUGxvbWJvbiIsImlhdCI6MTczOTIyNjAwMSwiZXhwIjoxNzM5NDg1MjAxfQ.1t90oisX4k2q_HkErNyZ58JBR4F1VkvZNPQZURb5Mk0'
-
   beforeEach(() =>{
     cy.visit('https://adopet-frontend-cypress.vercel.app/');
     cy.get('[data-test="login-button"]').click(); 
@@ -10,11 +8,11 @@ describe('exercicio 41', () => {
     cy.get('.button').click(); 
     })
 
-    it('Mensagens da Api', ()=>{
+    it('Perfil da Api', ()=>{
       cy.request({
           method: 'GET',
           url:'https://adopet-api-i8qu.onrender.com/adotante/perfil/c42b8c13-1474-47df-b4cd-ed87fce4195c',
-          headers:{authorization}    
+          headers: Cypress.env()  
       }).then((res) => {
           expect(res.status).to.be.equal(200)
           expect(res.body).is.not.empty
