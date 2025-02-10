8ce96acd8fe8801c8213c398da20c2326cf69fdd
theusvoltane <m.voltane@gmail.com>
2025-02-10 20:01:40 -0300

    teste de api com a pagina mensagem

--- cypress/e2e/api-mensagens.cy.js
+++ cypress/e2e/api-mensagens.cy.js
@@ -1,11 +1,11 @@
 describe('Api Adopet', ()=>{
-
-    const authorization = 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJjNDJiOGMxMy0xNDc0LTQ3ZGYtYjRjZC1lZDg3ZmNlNDE5NWMiLCJhZG9wdGVyTmFtZSI6Ik1hdGhldXMgUGxvbWJvbiIsImlhdCI6MTczODg3MzQ4MywiZXhwIjoxNzM5MTMyNjgzfQ.A1FP3DYS28fQ1rCiBAUlzpURh-kMvKY9a6sDnjPhXZg'
+    
+    const authorization = 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJjNDJiOGMxMy0xNDc0LTQ3ZGYtYjRjZC1lZDg3ZmNlNDE5NWMiLCJhZG9wdGVyTmFtZSI6Ik1hdGhldXMgUGxvbWJvbiIsImlhdCI6MTczOTIyNjAwMSwiZXhwIjoxNzM5NDg1MjAxfQ.1t90oisX4k2q_HkErNyZ58JBR4F1VkvZNPQZURb5Mk0'
     
     it('Mensagens da Api', ()=>{
         cy.request({
             method: 'GET',
-            url:'https://adopet-api-i8qu.onrender.com/adotante/login',
+            url:'https://adopet-api-i8qu.onrender.com/mensagem/c42b8c13-1474-47df-b4cd-ed87fce4195c',
             headers:{authorization}    
         }).then((res) => {
             expect(res.status).to.be.equal(200)
